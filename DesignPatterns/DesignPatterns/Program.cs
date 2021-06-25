d03f6d26dff553c3f78dfc3c23c55ea688fe1769
lucasamacedo <lucas_am_@hotmail.com>
2021-06-25 08:55:43 -0300

    Adicionando padrão Chain of Responsibility

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -6,6 +6,8 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
+            // aula 1
+
             //Imposto iss = new ISS();
             //Imposto icms = new ICMS();
             //Imposto iccc = new ICCC();
@@ -24,18 +26,33 @@ namespace DesignPatterns
             //orcamento = new Orcamento(3500.0);
             //calculador.RealizaCalculo(orcamento, iccc);
 
+            // Exercicios aula 1
 
-            Investimento inv_c = new Conservador();
-            Investimento inv_m = new Moderado();
-            Investimento inv_a = new Arrojado();
-            Conta conta = new Conta();
-            RealizadorDeInvestimentos investidor = new RealizadorDeInvestimentos();
-            conta.Deposita(500);
+            //Investimento inv_c = new Conservador();
+            //Investimento inv_m = new Moderado();
+            //Investimento inv_a = new Arrojado();
+            //Conta conta = new Conta();
+            //RealizadorDeInvestimentos investidor = new RealizadorDeInvestimentos();
+            //conta.Deposita(500);
 
-            investidor.Realiza(conta, inv_c);
-            investidor.Realiza(conta, inv_m);
-            investidor.Realiza(conta, inv_a);
+            //investidor.Realiza(conta, inv_c);
+            //investidor.Realiza(conta, inv_m);
+            //investidor.Realiza(conta, inv_a);
 
+            // aula 2
+
+            CalculadorDeDescontos calculador = new CalculadorDeDescontos();
+
+            Orcamento orcamento = new Orcamento(500);
+            orcamento.AdicionaItem(new Item("CANETA", 500));
+            orcamento.AdicionaItem(new Item("LAPIS", 500));
+            orcamento.AdicionaItem(new Item("GELADEIRA", 500));
+            orcamento.AdicionaItem(new Item("FOGAO", 500));
+            orcamento.AdicionaItem(new Item("VENTILADOR", 500));
+            orcamento.AdicionaItem(new Item("PIA", 500));
+
+            double desconto = calculador.Calcula(orcamento);
+            Console.WriteLine(desconto);
         }
     }
 }
