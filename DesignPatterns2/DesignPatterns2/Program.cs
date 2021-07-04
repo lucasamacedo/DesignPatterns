eb3e2ea90f6ed701c25b6f2b32d91e02d7727b03
lucasamacedo <lucas_am_@hotmail.com>
2021-07-04 09:46:59 -0300

    Implementando Aula 2: Flyweight

--- DesignPatterns2/DesignPatterns2/Program.cs
+++ DesignPatterns2/DesignPatterns2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DesignPatterns2.Cap1;
+using DesignPatterns2.Cap2;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -9,10 +11,45 @@ namespace DesignPatterns2
     {
         static void Main(string[] args)
         {
-            IDbConnection conexao = new ConnectionFactory().GetConnection();
+            //// Aula 1
+            //IDbConnection conexao = new ConnectionFactory().GetConnection();
 
-            IDbCommand comando = conexao.CreateCommand();
-            comando.CommandText = "SELECT * FROM TABELA";
+            //IDbCommand comando = conexao.CreateCommand();
+            //comando.CommandText = "SELECT * FROM TABELA";
+
+            NotasMusicais notas = new NotasMusicais();
+            IList<INota> musica = new List<INota>()
+            {
+                notas.Pega("do"),
+                notas.Pega("re"),
+                notas.Pega("mi"),
+                notas.Pega("fa"),
+                notas.Pega("fa"),
+                notas.Pega("fa"),
+
+                notas.Pega("do"),
+                notas.Pega("re"),
+                notas.Pega("do"),
+                notas.Pega("re"),
+                notas.Pega("re"),
+                notas.Pega("re"),
+
+                notas.Pega("do"),
+                notas.Pega("sol"),
+                notas.Pega("fa"),
+                notas.Pega("mi"),
+                notas.Pega("mi"),
+                notas.Pega("mi"),
+
+                notas.Pega("do"),
+                notas.Pega("re"),
+                notas.Pega("mi"),
+                notas.Pega("fa"),
+                notas.Pega("fa"),
+                notas.Pega("fa")
+            };
+            Piano piano = new Piano();
+            piano.Toca(musica);
         }
     }
 }
